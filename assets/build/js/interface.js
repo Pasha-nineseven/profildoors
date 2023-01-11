ac4e3e474670c5164a9efb3543d61d703ee74503
Pavel.Krakvin <pavel.krakvin@libertexgroup.com>
2023-01-11 22:41:38 +0300

    +

--- assets/build/js/interface.js
+++ assets/build/js/interface.js
@@ -1,13 +1,273 @@
 $(document).ready(function() {
-	$("body").on("click", ".js-test", function(e){
+	//menu-mobile
+	$("body").on("click", ".js-menu-toggle", function(e){
 		e.preventDefault();
-		console.log('test')
+		$(this).toggleClass('active');
+		$('.menu-mobile').toggleClass('active');
 	});
+	$(document).click( function(e){
+		var div = $( ".menu-toggle-view" );
+		if ( !div.is(e.target)
+		    && div.has(e.target).length === 0 ) {
+			$('.menu-mobile').removeClass('active');
+			$('.menu-toggle').removeClass('active');
+		}
+	});
+
+
+	//index-top-slider
+	if($('.home-slider').length>0){
+		var slideCount = $('.slideCount');
+		var slickSlide = $('.home-slider');
+	
+		slickSlide.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+			var i = (currentSlide ? currentSlide : 0) + 1;
+			$(this).find('.slideCount').html('<span class="slideCountItem">' + i + '</span> ' + '/' + ' <span class="slideCountAll">' + slick.slideCount + '</span>');
+		});
+	
+		slickSlide.slick({
+			slide: '.home-slider__item',
+			slideToShow: 1,
+			fade: true,
+			arrows:true,
+			nextArrow: '.next-btn',
+  			prevArrow: '.prev-btn'
+		});
+	}
+	
+	//gallery
+	$(".js-gallery").fancybox({
+        speed : 330,
+        transitionEffect: "slide", 
+        animationEffect: "zoom-in-out", 
+        infobar: true,
+        idleTime: true,
+        toolbar  : true,
+        image : {
+            protect : true,
+        },
+    });
+
+	//POPUP-INLINE
+    $(".js-popup-inline").fancybox({
+        speed : 330,
+        transitionEffect: "slide", 
+        animationEffect: "zoom-in-out", 
+        infobar: true,
+        idleTime: true,
+        toolbar  : true,
+    });
+
+	//index-top-slider
+	if($('.gallery-slider').length>0){
+		var $g_slider = $('.gallery-slider');
+
+	    $g_slider.slick({
+			speed: 250,
+			swipe: true,
+			swipeToSlide: true,
+			touchThreshold: 10,
+			arrows:true,
+			dots:false,
+			useTransform:true,
+			accessibility: false,
+			infinite: true,
+			slidesToShow: 1,
+  			slidesToScroll: 1,
+	    });
+	}
+
+	//tabs
+	if($('#series-tabs').length>0){
+		var $tabs = $('#series-tabs');
+		$tabs.responsiveTabs({
+			rotate: false,
+			startCollapsed: 'accordion',
+			collapsible: 'accordion',
+			setHash: false,
+		});
+	}
+	
+
+	//video-play
+	$("body").on("click", ".js-system-view__video", function(e){
+		e.preventDefault();
+		$(this).parents('.system-view').find('.videoPopUpBox').show();
+		$(this).parents('.system-view').find('.video-js').trigger('play');
+	});
+	//video-close
+	$("body").on("click", ".videoPopUpBox", function(e){
+		e.preventDefault();
+		$(this).hide();
+		$(this).parents('.system-view').find('.video-js').trigger('pause');
+	});
+
+	//map
+	if($('#page-map').length>0){
+		ymaps.ready(function(){
+			initMap1();
+		});
+	}
+
+	//map-toggle
+	$("body").on("click", ".js-b-where-map-toggle", function(e){
+		e.preventDefault();
+		$('.page-map').addClass('open');
+	});
+	$("body").on("click", ".js-b-where-map-close", function(e){
+		e.preventDefault();
+		$('.page-map').removeClass('open');
+	});
+
+
+	//select
+	if($('.js-chosen').length>0){
+		$('.js-chosen').chosen({
+			width: '100%',
+			no_results_text: 'Совпадений не найдено',
+			placeholder_text_single: 'Любой'
+		});
+	}
+
+
+
+	//home-news-mobile
+	slider_news_init();
+	slider_doors_init();
+	slider_other_init();
 });
 
-
+$(window).resize(function () {
+	//home-news-mobile
+	slider_news_init();
+	slider_doors_init();
+	slider_other_init();
+});
 
 // functions
-function test(item){
-  	
+function slider_news_init(){
+	var $s_news = $('.home-news__list');
+	if ($(window).width() < 753) {
+		$s_news.not('.slick-initialized').slick({
+			infinite: true,
+			dots: false,
+			arrows: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			responsive: [
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+					}
+				},
+			]
+		});
+	} else {
+		if ($s_news.hasClass('slick-initialized')) {
+			$s_news.slick("unslick");
+		}
+	}
 }
+
+
+function slider_doors_init(){
+	var $s_doors = $('.home-doors__list');
+	if ($(window).width() < 753) {
+		$s_doors.not('.slick-initialized').slick({
+			infinite: true,
+			dots: false,
+			arrows: true,
+			slidesToShow: 5,
+			slidesToScroll: 1,
+			responsive: [
+				{
+					breakpoint: 700,
+					settings: {
+						slidesToShow: 4,
+						slidesToScroll: 1,
+					}
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 1,
+					}
+				},
+			]
+		});
+	} else {
+		if ($s_doors.hasClass('slick-initialized')) {
+			$s_doors.slick("unslick");
+		}
+	}
+}
+
+function slider_other_init(){
+	var $s_other = $('.series__in__list');
+	if ($(window).width() < 753) {
+		$s_other.not('.slick-initialized').slick({
+			infinite: true,
+			dots: false,
+			arrows: true,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			responsive: [
+				{
+					breakpoint: 700,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1,
+					}
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1,
+					}
+				},
+			]
+		});
+	} else {
+		if ($s_other.hasClass('slick-initialized')) {
+			$s_other.slick("unslick");
+		}
+	}
+}
+
+function initMap1() {
+	var myMap = new ymaps.Map("page-map", {
+        center:[53.9324054,27.6511743],
+        zoom: 7,
+        controls: []
+    }); 
+            
+    var myPlacemark = new ymaps.Placemark([53.9324054,27.6511743],{
+        	
+        },{
+        iconLayout: 'default#image',
+	});    
+
+    myMap.geoObjects.add(myPlacemark);
+	myMap.controls.add(new ymaps.control.ZoomControl({options: { position: { right: 10, top: 50 }}}));
+    myMap.behaviors.disable('scrollZoom')  
+}
+
+
+$(function() {
+	$('.tabs-nav__item').click(function(e) {
+		e.preventDefault();
+
+		$('.tabs-nav__item').removeClass('active');
+		$(this).addClass('active');
+
+		//active tab
+		let currentTab = $(this).attr('href');
+		$('.tabs-content__item').hide();
+		$(currentTab).show();
+
+	});
+});
--- assets/src/js/pages.js
+++ assets/src/js/pages.js
@@ -3,12 +3,22 @@ $('body').append(
 	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
 		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
 	<style> \
-		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
+		#pages { padding: 10px 20px 0 50px; font-size: 16px; } \
 		#pages a { text-decoration: none; } \
-		#pages li { margin: 5px 0; } \
+		#pages li { margin: 1px 0; } \
 	</style> \
 	<ol id="pages"> \
 		<li><a href="index.html">Index</a></li> \
-		<li><a href="article.html">Article</a></li> \
+		<li><a href="catalog.html">catalog</a></li> \
+		<li><a href="materials.html">materials</a></li> \
+		<li><a href="materials-in.html">materials-in</a></li> \
+		<li><a href="seriya.html">seriya</a></li> \
+		<li><a href="seriya-in.html">seriya-in</a></li> \
+		<li><a href="tv.html">TV</a></li> \
+		<li><a href="support.html">support</a></li> \
+		<li><a href="support2.html">support2</a></li> \
+		<li><a href="systems-in.html">systems-in</a></li> \
+		<li><a href="findings-in.html">findings-in</a></li> \
+		<li><a href="where.html">where</a></li> \
 	</ol> \
 </div>');
